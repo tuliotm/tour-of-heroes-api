1c0746fc6bac95f7e63a78e02016adf839d04dcd
tuliotm <tulioteodoromanso@gmail.com>
2023-05-11 15:00:45 -0300

    Add column token on heroes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_09_202710) do
+ActiveRecord::Schema.define(version: 2023_05_11_174951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2023_05_09_202710) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
+    t.index ["token"], name: "index_heroes_on_token"
   end
 
 end
